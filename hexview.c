179b228ec92ec3ba591c51e5b3d5a90711fc0397
bitboody <bodebi639@gmail.com>
2025-03-28 08:10:34 +0200

    Add hex viewer implementation to read and display binary file contents

---
+++ hexview.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char file_dist[] = "a.exe";
+    FILE *fptr;
+    size_t bytes_read;
+    size_t offset = 0;
+    size_t buffer_size = 16;
+
+    unsigned char *buffer = malloc(buffer_size);
+    if (!buffer)
+    {
+        printf("Memory allocation failed\n");
+        return -1;
+    }
+
+    fptr = fopen(file_dist, "rb");
+    if (!fptr)
+    {
+        printf("%s not found.\n", file_dist);
+        free(buffer);
+        return -1;
+    }
+
+    while ((bytes_read = fread(buffer, 1, buffer_size, fptr)) > 0)
+    {
+        for (size_t i = 0; i < bytes_read; i++)
+        {
+            if (offset % 16 == 0)
+            {
+                printf("%08zX   ", offset);
+            }
+            printf("%02X ", buffer[i]);
+            offset++;
+            if (offset % 16 == 0)
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    if (offset % 16 != 0)
+    {
+        printf("\n");
+    }
+
+    fclose(fptr);
+    free(buffer);
+    return 0;
+}
